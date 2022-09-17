ff23687eccc8c4258e2260e6939f8c8b1a263118
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-17 09:00:26 -0300

    media aritimetica

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double numero1, numero2, numero3, media;
+Console.WriteLine("media aritimetica... ");
+
+Console.Write("Digite o Primeiro Numero... ");
+numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o Segundo Numero... ");
+numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o Terceiro Numero... ");
+numero3 = Convert.ToDouble(Console.ReadLine());
+
+media = (numero1 + numero2 + numero3) / 3;
+
+Console.WriteLine($"n/media : {media:N1}");
+
